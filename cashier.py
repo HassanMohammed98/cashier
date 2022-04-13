87acd56325496634acd139f4f3032b3e09fbfb37
Hassan Mohammed <hassan-n98@hotmail.com>
2022-04-13 04:50:48 +0300

    finish cashier -Task 4

--- cashier.py
+++ cashier.py
@@ -11,14 +11,21 @@ def get_invoice_items(items):
     # ['1 Apple 0.200KD', '4 Orange 1.200KD']
     # ---
     # Write your code here
-    ...
-
+    # ...
+    invoice_items = []
+    for item in items:
+        invoice_items.append("{} {} {:.3f}KD".format(item['quantity'], item['name'], (item['price']*item['quantity'])))
+    return invoice_items
 
 def get_total(items):
     # Items is a dictionary with a quantity and price key
     # Calculate the total of all items in the cart
     # Write your code here
-    ...
+    # ...
+    total = 0
+    for item in items:
+        total += (item['price']*item['quantity'])
+    return "{:.3f}".format(total)
 
 
 def print_receipt(invoice_items, total):
@@ -28,12 +35,34 @@ def print_receipt(invoice_items, total):
     # total at the end.
     # ---
     # Write your code here
-    ...
-
+    # ...
+    print("\n-------------------\nreceipt\n-------------------")
+    for item in invoice_items:
+        # total += (item['price']*item['quantity'])
+        print(item)
+    print("-------------------\n Total Price: {}KD".format(total))
 
 def main():
     # Write your main logic here
-    ...
+    # ...
+    items = []
+    while True:
+        Item = input("Item (enter \"done\" when finished): ")
+        if (Item.casefold() != "done"):
+            while True:
+                try:
+                    Price = float(input("Price: "))
+                    Quantity = int(input("Quantity: "))
+                except:
+                    print("inputs are invalid. try again.")
+                else:
+                    items.append({'name': Item, 'quantity': Quantity, 'price': Price })
+                    break
+        else:
+            invoice_items = get_invoice_items(items)
+            total = get_total(items)
+            print_receipt(invoice_items, total)
+            break
 
 
 if __name__ == "__main__":
